bfee1169de3d37faddcdc703b32ec1b51cf3dace
ay369 <1032871936@qq.com>
2019-05-14 22:08:44 +0800

    项目的基本搭建

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <!-- 出口 -->
+    <router-view/>
+    <FooterGuide/>
+  </div>
+</template>
+
+<script>
+/* eslint-disable-next-line */
+import FooterGuide from "./components/FooterGuide/FooterGuide";
+export default {
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+  width: 100%
+  height: 100%
+  background: #f5f5f5
+</style>
